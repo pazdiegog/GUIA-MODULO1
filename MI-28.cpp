1bc159094b7f27b7c8ebad6817568998a13cdeb6
Diego Paz <paz.diegog@gmail.com>
2017-06-14 22:46:23 -0300

    Add files via upload

---
+++ MI-28.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main()
+{
+        string oracion;
+        int cantidadPalabras,cantidadVocales, letrasPorPalabra,espacios,palabraMayor,siguientePalabra;
+        espacios=0;
+        palabraMayor=0;
+        letrasPorPalabra=0;
+        cantidadVocales=0;
+        cantidadPalabras=0;
+
+        cout << "Ingrese una oracion: ";
+        getline(cin,oracion);
+
+        for (int i=0; i<=oracion.size();i++)
+        {
+            if ((oracion[i]=='a') || (oracion[i]=='e') || (oracion[i]=='i') ||(oracion[i]=='o') || (oracion[i]=='u'))
+                cantidadVocales++;//habria q ajustar con un contador para cada vocal, pero no hay ganas.
+            if ((oracion[i]!=' ') && (i<oracion.size()))
+                letrasPorPalabra++;
+            if (letrasPorPalabra==1)
+                ++cantidadPalabras;
+            if (letrasPorPalabra>palabraMayor)
+                palabraMayor=letrasPorPalabra;
+            if (oracion[i]==' ')
+                {
+
+                    ++espacios;
+                    letrasPorPalabra=0;
+                }
+        }
+        cout << "Cantidad de vocales: " << cantidadVocales << "\n";
+        cout << "Cantidad de espacios: " << espacios << "\n";
+
+        cout << "Cantidad de letras que posee la palabra mas larga: " << palabraMayor << "\n";
+}
